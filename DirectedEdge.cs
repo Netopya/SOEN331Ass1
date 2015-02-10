d087207abe9130db27d35afb20b82ac9c41f822a
Michael B <netopya@netopyaplanet.com>
2015-02-09 21:01:20 -0500

    Updated directed graph
    Updated to better reflect edge - element model

---
+++ DirectedEdge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOEN331Assignment1_2
+{
+    class DirectedEdge<E,V>
+    {
+        public E edge;
+        public V fromVertex;
+        public V toVertex;
+
+        public DirectedEdge(V fromVertex, V toVertex, E edge)
+        {
+            this.edge = edge;
+            this.fromVertex = fromVertex;
+            this.toVertex = toVertex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var testObj = obj as DirectedEdge<E, V>;
+            if (testObj != null)
+            {
+                return testObj.edge.Equals(edge) && testObj.fromVertex.Equals(fromVertex) && testObj.toVertex.Equals(toVertex);
+            }
+            return base.Equals(obj);
+        }
+    }
+}
--- DirectedGraph.cs
+++ DirectedGraph.cs
@@ -13,35 +13,11 @@ namespace SOEN331Assignment1_2
     /// <typeparam name="V">Vertex Type</typeparam>
     class DirectedGraph<E,V>
     {
-        private List<Node<E,V>> set;
-
-        private class Node<E, V>
-        {
-            public E edge;
-            public V fromVertex;
-            public V toVertex;
-
-            public Node(V fromVertex, V toVertex, E edge)
-            {
-                this.edge = edge;
-                this.fromVertex = fromVertex;
-                this.toVertex = toVertex;
-            }
-
-            public override bool Equals(object obj)
-            {
-                var testObj = obj as Node<E,V>;
-                if (testObj != null)
-                {
-                    return testObj.edge.Equals(edge) && testObj.fromVertex.Equals(fromVertex) && testObj.toVertex.Equals(toVertex);
-                }
-                return base.Equals(obj);
-            }
-        }
-
+        private List<DirectedEdge<E,V>> set;
+        
         public DirectedGraph()
         {
-            set = new List<Node<E,V>>();
+            set = new List<DirectedEdge<E,V>>();
         }
 
         public DirectedGraph<E, V> newdirectedgraph()
@@ -51,7 +27,7 @@ namespace SOEN331Assignment1_2
 
         public DirectedGraph<E,V> insertDirectedEdge(V v, V w, E x)
         {
-            Node<E, V> newNode = new Node<E, V>(v, w, x);
+            DirectedEdge<E, V> newNode = new DirectedEdge<E, V>(v, w, x);
             if (!set.Any(a => a.Equals(newNode)))
             {
                 set.Add(newNode);
@@ -60,9 +36,9 @@ namespace SOEN331Assignment1_2
             return this;
         }
 
-        public HashSet<E> incomingEdgesOf(V v)
+        public HashSet<DirectedEdge<E, V>> incomingEdgesOf(V v)
         {
-            return new HashSet<E>(set.Where(x => x.toVertex.Equals(v)).Select(x => x.edge));
+            return new HashSet<DirectedEdge<E, V>>(set.Where(x => x.toVertex.Equals(v)));
         }
 
         public int inDegreeOf(V v)
@@ -75,9 +51,9 @@ namespace SOEN331Assignment1_2
             return set.Where(x => x.fromVertex.Equals(v)).Count();
         }
 
-        public HashSet<E> outgoingEdgesOf(V v)
+        public HashSet<DirectedEdge<E, V>> outgoingEdgesOf(V v)
         {
-            return new HashSet<E>(set.Where(x => x.fromVertex.Equals(v)).Select(x => x.edge));
+            return new HashSet<DirectedEdge<E, V>>(set.Where(x => x.fromVertex.Equals(v)));
         }
     }
 }
